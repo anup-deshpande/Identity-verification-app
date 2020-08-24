394d91e39df03c46cc174869095b60be41b4ef6c
Anup Deshpande <deshpande.anup24@gmail.com>
2020-08-24 13:28:42 -0400

    Initial Commit

---
+++ identity-verification-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  identity-verification-app
+//
+//  Created by Anup Deshpande on 8/24/20.
+//  Copyright © 2020 Anup Deshpande. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
